be8b5dcaf4449f74088fb7995b370a425ef55fc6
Vladyslav Boychuk <boychuk.126836@e.unavarra.es>
2021-04-28 20:40:59 +0200

    [] - funcionamiento pagina inicio

--- vista.php
+++ vista.php
@@ -1,6 +1,12 @@
 <?php
     function vmostrarPaginaPrincipal(){
-        echo file_get_contents("header.html");
+
+        $header = file_get_contents("header.html");
+		$footer = file_get_contents("footer.html");
+        $principal = file_get_contents("principal.html");
+		$principal = str_replace("##header##", $header, $principal);
+        $principal = str_replace("##footer##", $footer, $principal);
+        echo $principal;
     }
 
 ?>
